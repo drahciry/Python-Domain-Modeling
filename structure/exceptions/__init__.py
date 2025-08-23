eb4e5797cbb316550d21eb487c3d7d1b3da7348e
drahciry <richardgonric@gmail.com>
2025-08-23 19:49:51 -0300

    Update module

--- structure/exceptions/__init__.py
+++ structure/exceptions/__init__.py
@@ -1,8 +1,11 @@
 # All custom exceptions classes
 from .InvalidCategoryError import InvalidCategoryError
 from .InvalidQuantityError import InvalidQuantityError
+from .InvalidAddressError import InvalidAddressError
 from .InvalidEmailError import InvalidEmailError
 from .InvalidPriceError import InvalidPriceError
+from .InvalidStateError import InvalidStateError
+from .InvalidCityError import InvalidCityError
 from .InvalidNameError import InvalidNameError
 from .InvalidPathError import InvalidPathError
 from .InvalidIdError import InvalidIdError
@@ -11,8 +14,11 @@ from .InvalidIdError import InvalidIdError
 __all__ = [
     'InvalidCategoryError',
     'InvalidQuantityError',
+    'InvalidAddressError',
     'InvalidEmailError',
     'InvalidPriceError',
+    'InvalidStateError',
+    'InvalidCityError',
     'InvalidNameError',
     'InvalidPathError',
     'InvalidIdError'
